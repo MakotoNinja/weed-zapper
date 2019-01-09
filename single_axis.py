496ed574b7cfe56d2368c2b539e3f631432f9f73
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-08 14:41:33 -1000

    added manifest and python script

---
+++ single_axis.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""Set servo angle."""
+
+import os
+import json
+import requests
+
+def get_env(key, type_=int):
+	'Return the value of the namespaced Farmware input variable.'
+	return type_(os.environ['{}_{}'.format(farmware_name, key)])
+
+def single_axis(axis, pos):
+	'Wrap the data in a `set_servo_angle` Celery Script command to send it.'
+	return {
+		"kind": "set_servo_angle",
+		"args": {"pin_number": pin, "pin_value": angle}
+	}
+
+def post(wrapped_data):
+	'Send the Celery Script command.'
+	headers = {
+		'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+		'content-type': "application/json"
+	}
+	payload = json.dumps(wrapped_data)
+	requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
+	data=payload, headers=headers)
+
+	if __name__ == "__main__":
+		farmware_name = 'single_axis'
+		post(set_servo_angle(get_env('pin_number'), get_env('servo_angle')))
