6daf49cb2d0f3392f0d6c1d1411b4e64b3c31c42
MakotoNinja <MakotoNinja@users.noreply.github.com>
2019-01-09 12:58:05 -1000

    Update single_axis.py

--- single_axis.py
+++ single_axis.py
@@ -6,4 +6,11 @@
 
 from farmware_tools import device
 
-device.log('Hello Farm.', 'info')
+import requests
+import os
+
+API_TOKEN = os.environ['API_TOKEN']
+
+headers = {'Authorization': 'Bearer ' + API_TOKEN, 'content-type': 'application/json'}
+log = {'message': 'Hello Farm!', 'type': 'info'}
+response = requests.post('https://my.farmbot.io/api/logs', headers=headers, json=log)
