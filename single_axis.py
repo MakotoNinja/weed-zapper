1352a0e38eb61da3a3d08c312ef9443e85ee142e
MakotoNinja <MakotoNinja@users.noreply.github.com>
2019-01-09 13:00:17 -1000

    Update single_axis.py

--- single_axis.py
+++ single_axis.py
@@ -10,7 +10,8 @@ import requests
 import os
 
 API_TOKEN = os.environ['API_TOKEN']
+FARMWARE_URL = os.environ['FARMWARE_URL']
 
 headers = {'Authorization': 'Bearer ' + API_TOKEN, 'content-type': 'application/json'}
 log = {'message': 'Hello Farm!', 'type': 'info'}
-response = requests.post('https://my.farmbot.io/api/logs', headers=headers, json=log)
+response = requests.post(FARMWARE_URL, headers=headers, json=log)
