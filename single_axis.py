c6d5cab1581308f96fd0438116fab8d856f0514c
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-08 15:35:21 -1000

    Updated single_axis.py

--- single_axis.py
+++ single_axis.py
@@ -1,34 +1,9 @@
 #!/usr/bin/env python
 
-"""Set servo angle."""
+'''
+ ' Single Axis
+'''
 
-import os
-import json
-import requests
 from farmware_tools import device
 
-def get_env(key, type_=int):
-	'Return the value of the namespaced Farmware input variable.'
-	return type_(os.environ['{}_{}'.format(farmware_name, key)])
-
-def single_axis(axis, pos):
-	'Wrap the data in a `single_axis` Celery Script command to send it.'
-	return {
-		"kind": "single_axis",
-		"args": {"axis": axis, "pos": value}
-	}
-
-def post(wrapped_data):
-	'Send the Celery Script command.'
-	headers = {
-		'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-		'content-type': "application/json"
-	}
-	payload = json.dumps(wrapped_data)
-	requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
-	data=payload, headers=headers)
-
-if __name__ == "__main__":
-	farmware_name = 'single_axis'
-	#post(set_servo_angle(get_env('axis'), get_env('position')))
-	device.log('Bot is at position {{ x }}, {{ y }}, {{ z }}.', 'success', ['toast'])
+device.log('Bot is at position {{ x }}, {{ y }}, {{ z }}.', 'success', ['toast'])
