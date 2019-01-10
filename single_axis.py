52e9acefd0b80436be99a54e97d325f41cf5953b
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 12:28:43 -1000

    added bot state

--- single_axis.py
+++ single_axis.py
@@ -12,5 +12,6 @@ from farmware_tools import get_config_value
 x=y=z=0
 axis = get_config_value('Single Axis', 'axis', str)
 pos = get_config_value('Single Axis', 'pos')
-log = 'Axis: {}, Position: {}'.format(axis, pos)
+bot_state = device.get_bot_state()
+log = 'Axis: {}, Position: {}, State: {}'.format(axis, pos, bot_state)
 device.log(log, 'info', ['toast'])
