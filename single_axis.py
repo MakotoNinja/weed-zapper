5d13dc3c06239ffa815f73b00ce0838581ed0529
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 13:06:36 -1000

    using assebme_coordinate

--- single_axis.py
+++ single_axis.py
@@ -9,12 +9,12 @@ from farmware_tools import device
 from farmware_tools import app
 from farmware_tools import get_config_value
 
-positions = {'x' : 0, 'y' : 0, 'z' : 0}
-for position in positions:
-	positions[position] = device.get_current_position(position)
+coord = device.assemble_coordinate(0, 0, 0)
+#for position in positions:
+#	positions[position] = device.get_current_position(position)
 
-axis = get_config_value('Single Axis', 'axis', str).lower()
-positions[axis] = int(get_config_value('Single Axis', 'pos'))
+#axis = get_config_value('Single Axis', 'axis', str).lower()
+#positions[axis] = int(get_config_value('Single Axis', 'pos'))
 
-log = 'Axis: {}, Position: {}, State: {}'.format(axis, pos, positions)
+log = 'Axis: {}, Position: {}, State: {}'.format(axis, pos, coord)
 device.log(log, 'info', ['toast'])
