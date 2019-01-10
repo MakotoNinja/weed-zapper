b3b07d373124fb250d6e5ca489cae2358c0c6596
MakotoNinja <MakotoNinja@users.noreply.github.com>
2019-01-10 10:02:07 -1000

    Update single_axis.py

--- single_axis.py
+++ single_axis.py
@@ -4,7 +4,12 @@
  ' Single Axis
 '''
 
+import os
 from farmware_tools import device
 from farmware_tools import app
 
-device.log('Bot is at position {{ x }}, {{ y }}, {{ z }}.', 'success', ['toast'])
+x=y=z=0
+axis = os.environ.get('axis')
+position = os.environ.get('pos')
+log = 'Axis: {}, Position:{}'.format(axis, position)
+device.log(log, 'info', ['toast'])
