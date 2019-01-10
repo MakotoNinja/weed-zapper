a72e4884db4525eb570be9650707ab24fe9f981c
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 09:21:44 -1000

    bare bones

--- single_axis.py
+++ single_axis.py
@@ -6,6 +6,4 @@
 
 from farmware_tools import device
 
-device.log('Hello Farm.', 'info', ['toast'])
-device.move_relative(x=0, y=100, z=0, speed=100)
-device.log('Bot is at position {{ x }}, {{ y }}, {{ z }}.')
+device.log('Hello Farm.')
