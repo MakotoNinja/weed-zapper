22c5bdfd6033158a3eba1aea40d32bfe67031869
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 09:17:23 -1000

    removed extra period

--- single_axis.py
+++ single_axis.py
@@ -7,5 +7,5 @@
 from farmware_tools import device
 
 device.log('Hello Farm.', 'info', ['toast'])
-device.move_relative(x=0, y=100, z=0, speed=100).
+device.move_relative(x=0, y=100, z=0, speed=100)
 device.log('Bot is at position {{ x }}, {{ y }}, {{ z }}.')
