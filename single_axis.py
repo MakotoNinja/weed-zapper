0475727ecbbda26fddb17a9fc25b61d81c7901b3
MakotoNinja <MakotoNinja@users.noreply.github.com>
2019-01-10 06:40:14 -1000

    Update single_axis.py

--- single_axis.py
+++ single_axis.py
@@ -6,12 +6,4 @@
 
 from farmware_tools import device
 
-import requests
-import os
-
-API_TOKEN = os.environ['API_TOKEN']
-FARMWARE_URL = os.environ['FARMWARE_URL']
-
-headers = {'Authorization': 'Bearer ' + API_TOKEN, 'content-type': 'application/json'}
-log = {'message': 'Hello Farm!', 'type': 'info'}
-response = requests.post(FARMWARE_URL, headers=headers, json=log)
+device.log('Hello Farm.', 'info', ['toast'])
