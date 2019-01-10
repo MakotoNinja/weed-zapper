6451ec8e7d5509e2b02ec6d58cfae62a9d990662
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 12:49:53 -1000

    get current position

--- single_axis.py
+++ single_axis.py
@@ -14,7 +14,7 @@ for position in positions:
 	positions[position] = device.get_current_position(position)
 
 axis = get_config_value('Single Axis', 'axis', str).lower()
-pos = get_config_value('Single Axis', 'pos')
+posistions[axis] = get_config_value('Single Axis', 'pos')
 
 log = 'Axis: {}, Position: {}, State: {}'.format(axis, pos, positions)
 device.log(log, 'info', ['toast'])
