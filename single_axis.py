e09270e62e537565e67752fd838d88e73c03d910
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 13:18:20 -1000

    using assebme_coordinate

--- single_axis.py
+++ single_axis.py
@@ -13,7 +13,7 @@ coord = device.assemble_coordinate(0, 0, 0)
 #for position in positions:
 #	positions[position] = device.get_current_position(position)
 
-#axis = get_config_value('Single Axis', 'axis', str).lower()
+axis = get_config_value('Single Axis', 'axis', str).lower()
 #positions[axis] = int(get_config_value('Single Axis', 'pos'))
 
 log = 'Axis: {}, Coordinate: {}'.format(axis, coord)
