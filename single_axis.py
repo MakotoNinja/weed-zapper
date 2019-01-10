2577a84adf2b287ff553b78c9822c2a6de1570f4
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 10:38:45 -1000

    hard code vars for test

--- single_axis.py
+++ single_axis.py
@@ -10,7 +10,7 @@ from farmware_tools import app
 from farmware_tools import get_config_value
 
 x=y=z=0
-axis = get_config_value(farmware_name='Single Axis', config_name='axis')
-pos = -5#get_config_value('Single Axis', 'pos')
+axis = 'X'#get_config_value('Single Axis', 'axis')
+pos = get_config_value('Single Axis', 'pos')
 log = 'Axis: {}, Position: {}'.format(axis, pos)
 device.log(log, 'info', ['toast'])
