659185f95eff001bbf560eca43e597705164bbf6
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 10:52:30 -1000

    added type to get_config_value call

--- single_axis.py
+++ single_axis.py
@@ -10,7 +10,7 @@ from farmware_tools import app
 from farmware_tools import get_config_value
 
 x=y=z=0
-axis = 'X'#get_config_value('Single Axis', 'axis')
+axis = get_config_value('Single Axis', 'axis', 'str')
 pos = get_config_value('Single Axis', 'pos')
 log = 'Axis: {}, Position: {}'.format(axis, pos)
 device.log(log, 'info', ['toast'])
