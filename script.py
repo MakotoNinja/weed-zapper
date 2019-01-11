f153d485df4fc58a1cf15c751e75f627abc398e2
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 16:47:00 -1000

    added all fields

--- script.py
+++ script.py
@@ -13,13 +13,19 @@ from random import randint
 # create Celery coordinate node
 coord = device.assemble_coordinate(0, 0, 0)
 
+rand_vals = {
+	'x' : {'lo' : 0, 'hi' : 0},
+	'y' : {'lo' : 0, 'hi' : 0},
+	'z' : {'lo' : 0, 'hi' : 0},
+}
 # get input values
-x_lo = int(get_config_value('Random Move Relative', 'x_lo'))
-x_hi = int(get_config_value('Random Move Relative', 'x_hi'))
-x_ran = randint(x_lo, x_hi)
+for axis in rand_vals:
+	rand_vals[axis]['lo'] = int(get_config_value('Random Move Relative', axis + '_lo'))
+ 	rand_vals[axis]['hi'] = int(get_config_value('Random Move Relative', axis + '_hi'))
+
 
 #log = 'Axis: {}, Coordinate: {}'.format(single_axis, coord)
-log = "Random X: {}".format(x_ran)
+log = "Random X: {}".format(rand_vals)
 device.log(log, 'info', ['toast'])
 # perform the move
 #device.move_absolute(coord, 100, device.assemble_coordinate(0, 0, 0))
