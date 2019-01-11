e220226eb4cef714d8c23814f317def67551833f
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-10 17:23:10 -1000

    updated script.py

--- script.py
+++ script.py
@@ -15,11 +15,13 @@ coord = device.assemble_coordinate(0, 0, 0)
 
 # get input values
 for axis in coords['args']:
-	x_lo = 5#int(get_config_value('Random Move Relative', (axis + '_lo')))
- 	x_hi = 10#int(get_config_value('Random Move Relative', (axis + '_hi')))
-	coord['args'][axis] = randint(x_lo, x_hi)
+	lo = int(get_config_value('Random Move Relative', (axis + '_lo')))
+ 	hi = int(get_config_value('Random Move Relative', (axis + '_hi')))
+	#coord['args'][axis] = randint(lo, hi)
+	log = axis.upper() + ': ' + randint(x_lo, x_hi)
+	device.log(log, 'info', ['toast'])
 
-log = "Random X: {}".format(coord)
-device.log(log, 'info', ['toast'])
+#log = "Random X: {}".format(coord)
+#device.log(log, 'info', ['toast'])
 # perform the move
 #device.move_relative(coord, 100, device.assemble_coordinate(0, 0, 0))
