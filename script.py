3969b0b437127fac1537dff83d9be40de9ab2326
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-12 12:31:28 -1000

    changed to json.dumps

--- script.py
+++ script.py
@@ -11,8 +11,8 @@ from farmware_tools import get_config_value
 points = app.get_points()
 plants = app.get_plants()
 
-log = "Points: {}".format(points)
+log = "Points: {}".format(json.dumps(points))
 device.log(log, 'info', ['toast'])
 
-log = "Plants: {}".format(plants)
+log = "Plants: {}".format(json.dumps(plants))
 device.log(log, 'info', ['toast'])
