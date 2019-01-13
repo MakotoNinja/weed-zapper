6a7381527023661ab9c66fe03f656350208cd3ea
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-13 12:54:04 -1000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-<h1>Point Data</h1
-<p>Playing with point data.</p>
+<h1>Weeder Routine</h1
+<p>A weeder routine for Farmbot.</p>
+<p>The weeder routine should:</p>
+<ul>
+	<li>Delete all point data.</li>
+	<li>Scan entire bed.</li>
+	<li>Stomp out any weeds that were detected.</li>
+</ul>
--- script.py
+++ script.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 '''
- ' Playing with points
+ ' Weeding Routine for Farmbot
 '''
 
 import os, json
 from farmware_tools import device, app, get_config_value
+
+X_MAX = get_config_value('Weeder Routine', 'x_max')
+Y_MAX = get_config_value('Weeder Routine', 'y_max')
+Z_MAX = get_config_value('Weeder Routine', 'z_max')
+
+X_MOVE = get_config_value('Weeder Routine', 'x_move')
+Y_MOVE = get_config_value('Weeder Routine', 'y_move')
 
 points = app.get_points()
 plants = app.get_plants()
@@ -28,5 +35,31 @@ def del_all_weeds(points):
 				device.log("App Error - Point ID: {}".format(point['id']), 'error')
 	return num_weeds
 
-del_all_points(points)
-device.sync()
+def mod_coord(coord, x, y, z):
+	coord['args']['x'] = x
+	coord['args']['y'] = y
+	coord['args']['z'] = z
+	return coord
+
+def get_pos(coord, axis):
+	return coord['args'][axis]
+
+def set_pos(coord, axis, value):
+	return coord['args'][axis]
+
+def weed_scan():
+	coord = device.assemble_coordinate(50, 50, 0)
+	offset = device.assemble_coordinate(0, 0, 0)
+	device.move_absolute(coord, 100, offset)
+	while get_pos(coord, 'y') < Y_MAX - Y_MOVE:
+		while get_pos(coord, 'x') < X_MAX - X_MOVE:
+			#device.execute_script(label = 'plant-detecttion')
+			set_pos(coord, 'x', get_pos(coord, 'x') + X_MOVE)
+			device.move_absolute(coord, 100, offset)
+		set_pos(coord, 'y', get_pos(coord, 'y') + Y_MOVE)
+		device.move_absolute(coord, 100, offset)
+	device.log('Scan Complete.', 'info', ['toast'])
+#del_all_points(points)
+#device.sync()
+
+#device.execute_script(label='my-farmware')
