85ba570d60ec32b008800256dd9e3f543d0948a3
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-13 13:52:18 -1000

    initial commit

---
+++ Coordinate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from farmware_tools import device, app
+
+class Coordinate:
+	def __init__(self, x=0, y=0, z=0):
+		self.coordinate = device.assemble_coordinate(x, y, z)
+
+	def set_pos(self, axis, pos):
+		self.coordinate['args'][axis] = pos
+
+	def get_pos(self, axis):
+		return self.coordinate['args'][axis]
+
+	def get_coordinate(self):
+		return self.coordinate['args']
+
+	def set_coordinate(x, y, z):
+		self.coordinate['args']['x'] = x
+		self.coordinate['args']['y'] = y
+		self.coordinate['args']['z'] = z
+
+	def get(self):
+		return self.coordinate
--- script.py
+++ script.py
@@ -6,14 +6,15 @@
 
 import os, json
 from farmware_tools import device, app, get_config_value
-
+from Coordinate import Coordinate
+"""
 X_MAX = get_config_value('Weeder Routine', 'x_max')
 Y_MAX = get_config_value('Weeder Routine', 'y_max')
 Z_MAX = get_config_value('Weeder Routine', 'z_max')
 
 X_MOVE = get_config_value('Weeder Routine', 'x_move')
 Y_MOVE = get_config_value('Weeder Routine', 'y_move')
-
+"""
 points = app.get_points()
 plants = app.get_plants()
 
@@ -35,31 +36,18 @@ def del_all_weeds(points):
 				device.log("App Error - Point ID: {}".format(point['id']), 'error')
 	return num_weeds
 
-def mod_coord(coord, x, y, z):
-	coord['args']['x'] = x
-	coord['args']['y'] = y
-	coord['args']['z'] = z
-	return coord
-
-def get_pos(coord, axis):
-	return coord['args'][axis]
-
-def set_pos(coord, axis, value):
-	coord['args'][axis] = value
-	return coord
-
 def weed_scan():
-	coord = device.assemble_coordinate(50, 50, 0)
+	coord = Coordinate(50,50)
 	offset = device.assemble_coordinate(0, 0, 0)
-	device.move_absolute(coord, 100, offset)
-	while get_pos(coord, 'y') < Y_MAX - Y_MOVE:
-		while get_pos(coord, 'x') < X_MAX - X_MOVE:
-			#device.execute_script(label = 'plant-detecttion')
-			coord = set_pos(coord, 'x', get_pos(coord, 'x') + X_MOVE)
-			device.move_absolute(coord, 100, offset)
-		coord = set_pos(coord, 'y', get_pos(coord, 'y') + Y_MOVE)
-		device.move_absolute(coord, 100, offset)
+	device.move_absolute(coord.get(), 100, offset)
+	while device.get_current_position('y') < Y_MAX - Y_MOVE:
+		while device.get_current_position('x') < X_MAX - X_MOVE:
+			coord.set_pos('x', coord.get_pos('x') + X_MOVE)
+			device.move_absolute(coord.get(), 100, offset)
+		coord.set_coordinate(50, coord.get_pos('y') + Y_MOVE)
+		device.move_absolute(coord.get(), 100, offset)
 	device.log('Scan Complete.', 'info', ['toast'])
+
 #del_all_points(points)
 #device.sync()
 
