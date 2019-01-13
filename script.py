9bb4d33c228d2de7ad4a8db1a14252794fd48044
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-12 15:32:13 -1000

    running func

--- script.py
+++ script.py
@@ -19,15 +19,15 @@ def del_all_points(points):
 
 def delete_all_weeds(points):
 	num_weeds = 0
-	for point in points:
+	"""for point in points:
 		if 'weed' in point['name'].lower():
 			try:
 				app.delete('points', point['id'])
 				weeds += 1
 			except:
-				device.log("App Error - Point ID: {}".format(point['id']))
+				device.log("App Error - Point ID: {}".format(point['id']))"""
 	return num_weeds
 
 deleted_weeds = del_all_weeds(points)
-#device.log('Deleted {} weed points.'.format(deleted_weeds))
+device.log('Deleted {} weed points.'.format(deleted_weeds))
 device.sync()
