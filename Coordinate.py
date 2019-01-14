fa33d8e9b4f1fc94904c7451a9909635bdf9e1f3
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-13 15:35:00 -1000

    init

--- Coordinate.py
+++ Coordinate.py
@@ -15,10 +15,13 @@ class Coordinate:
 	def get_coordinate(self):
 		return self.coordinate['args']
 
-	def set_coordinate(x=0, y=0, z=0):
+	def set_coordinate(self, x=0, y=0, z=0):
 		self.coordinate['args']['x'] = x
 		self.coordinate['args']['y'] = y
 		self.coordinate['args']['z'] = z
 
 	def get(self):
 		return self.coordinate
+
+	def print_me(self):
+		print(self.get_coordinate())
