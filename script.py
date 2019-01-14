55f39d19241e348428b0fb798ec779afcea90160
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-13 14:39:06 -1000

    init

--- script.py
+++ script.py
@@ -40,6 +40,8 @@ def weed_scan():
 	coord = Coordinate(50,50)
 	offset = device.assemble_coordinate(0, 0, 0)
 	device.move_absolute(coord.get(), 100, offset)
+	cp = device.get_current_position('y')
+	device.log('Y: {}, type: {}'.format(cp, type(cp)), 'info')
 	while device.get_current_position('y') < Y_MAX - Y_MOVE:
 		while device.get_current_position('x') < X_MAX - X_MOVE:
 			coord.set_pos('x', coord.get_pos('x') + X_MOVE)
