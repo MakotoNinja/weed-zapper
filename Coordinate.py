f71393124d7314d71e9d6bcd8f9833b357bd30d4
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-15 10:29:34 -1000

    Added offset coordinate

--- Coordinate.py
+++ Coordinate.py
@@ -3,8 +3,9 @@
 from farmware_tools import device, app
 
 class Coordinate:
-	def __init__(self, x=0, y=0, z=0):
+	def __init__(self, x=0, y=0, z=0, ox=0, oy=0, oz=0):
 		self.coordinate = device.assemble_coordinate(x, y, z)
+		self.offset = device.assemble_coordinate(ox, ox, oz)
 
 	def set_pos(self, axis, pos):
 		self.coordinate['args'][axis] = pos
@@ -22,6 +23,14 @@ class Coordinate:
 
 	def get(self):
 		return self.coordinate
+
+	def set_offset(self, x, y, z):
+		self.offset['args']['x'] = x
+		self.offset['args']['y'] = y
+		self.offset['args']['z'] = z
+
+	def get_offset(self):
+		return self.offset
 
 	def print_me(self):
 		print(self.get_coordinate())
